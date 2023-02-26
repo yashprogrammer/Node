6e6196244f0d12ea9a30c3669535e275ddf69088
iysh <yashstudy02@gmail.com>
2023-02-26 21:03:49 +0530

    Add input field and button to add new todo's

--- app.js
+++ app.js
@@ -3,22 +3,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
+var items = ["Wash Cloths", "Iron Them"];
 
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");  // this line used to apply ejs in express file  
 
 var today = new Date();
-var currentDay = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"];
+
+var options ={
+  weekday:"long",
+  year:"2-digit",
+  month:"short"
+}
+
+var day = today.toLocaleDateString("en-US",options);
+
+// var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"];
 
 app.get("/", function(req, res){
-  // res.send("Hello");
+  res.render("list",{Today:day, newListItems : items});
 
 
 
-  res.render("list",{Today:daylist[currentDay]})
+  // res.render("list",{Today:daylist[currentDay]})
 });
+
+app.post("/",function (req,res) {
+  var item = req.body.new_item;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
+
+
+
+
